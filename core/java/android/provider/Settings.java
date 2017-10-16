5048f18a98edefe59cdfb7d214b612f3653a5cab
Brett Rogers <brettrogers11@gmail.com>
2017-10-16 16:32:24 +0200

    Status bar notification ticker [1/2]
    * abc @ezio84: cleanup and port to Oreo
    * du @nychitman1: cleanup and port to Nougat
    
    Change-Id: Idfc5c3a9398d0590ea0ef32c8a6efbca8513a1e8
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3947,7 +3947,7 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
-       /**
+        /**
          * Whether to wake the display when plugging or unplugging the charger
          *
          * @hide
@@ -3955,7 +3955,7 @@ public final class Settings {
         public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
 
         /**
-         * Wheter to show network traffic indicator in statusbar
+         * Whether to show network traffic indicator in statusbar
          * @hide
          */
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
@@ -3965,6 +3965,12 @@ public final class Settings {
          * @hide
          */
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
+         * Whether to vibrate on power connection or disconnection
+         * @hide
+         */
+        public static final String VIBRATION_ON_CHARGE_STATE_CHANGED = "vibration_on_charge_state_changed";
 
         /**
          * Volume key controls ringtone or media sound stream
