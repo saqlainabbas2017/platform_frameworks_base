75354d2b0151b2c145f02ec6f0e1a0d9f158aed3
Amogh Lele <amolele@gmail.com>
2017-12-14 19:27:55 +0000

    Revert "Port ADB over network and ability to hide the notification"
    This reverts commit acca93192af44be0ec5b3a3448f89680502daefa.

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -66,8 +66,6 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
-import lineageos.providers.LineageSettings;
-
 /**
  * UsbDeviceManager manages USB state in device mode.
  */
@@ -377,7 +375,7 @@ public class UsbDeviceManager {
         private boolean mCurrentFunctionsApplied;
         private UsbAccessory mCurrentAccessory;
         private int mUsbNotificationId;
-        private int mAdbNotificationId;
+        private boolean mAdbNotificationShown;
         private int mCurrentUser = UserHandle.USER_NULL;
         private boolean mUsbCharging;
         private String mCurrentOemFunctions;
@@ -427,20 +425,6 @@ public class UsbDeviceManager {
                 mContentResolver.registerContentObserver(
                         Settings.Global.getUriFor(Settings.Global.ADB_ENABLED),
                         false, new AdbSettingsObserver());
-
-                ContentObserver adbNotificationObserver = new ContentObserver(null) {
-                    @Override
-                    public void onChange(boolean selfChange) {
-                        updateAdbNotification();
-                    }
-                };
-
-                mContentResolver.registerContentObserver(
-                        LineageSettings.Secure.getUriFor(LineageSettings.Secure.ADB_NOTIFY),
-                                false, adbNotificationObserver);
-                mContentResolver.registerContentObserver(
-                        LineageSettings.Secure.getUriFor(LineageSettings.Secure.ADB_PORT),
-                                false, adbNotificationObserver);
 
                 // Watch for USB configuration changes
                 mUEventObserver.startObserving(USB_STATE_MATCH);
@@ -1055,32 +1039,11 @@ public class UsbDeviceManager {
         private void updateAdbNotification() {
             if (mNotificationManager == null) return;
             final int id = SystemMessage.NOTE_ADB_ACTIVE;
-            boolean usbAdbActive = mAdbEnabled && mConnected;
-            boolean netAdbActive = mAdbEnabled &&
-                    LineageSettings.Secure.getInt(mContentResolver,
-                            LineageSettings.Secure.ADB_PORT, -1) > 0;
-            final int titleRes;
-            boolean hideNotification = "0".equals(SystemProperties.get("persist.adb.notify"))
-                    || LineageSettings.Secure.getInt(mContext.getContentResolver(),
-                            LineageSettings.Secure.ADB_NOTIFY, 1) == 0;
+            final int titleRes = com.android.internal.R.string.adb_active_notification_title;
+            if (mAdbEnabled && mConnected) {
+                if ("0".equals(SystemProperties.get("persist.adb.notify"))) return;
 
-            if (hideNotification) {
-                titleRes = 0;
-            } else if (usbAdbActive && netAdbActive) {
-                titleRes = com.android.internal.R.string.adb_both_active_notification_title;
-            } else if (usbAdbActive) {
-                titleRes = com.android.internal.R.string.adb_active_notification_title;
-            } else if (netAdbActive) {
-                titleRes = com.android.internal.R.string.adb_net_active_notification_title;
-            } else {
-                titleRes = 0;
-            }
-
-            if (titleRes != mAdbNotificationId) {
-                if (mAdbNotificationId != 0) {
-                    mNotificationManager.cancelAsUser(null, id, UserHandle.ALL);
-                }
-                if (titleRes != 0) {
+                if (!mAdbNotificationShown) {
                     Resources r = mContext.getResources();
                     CharSequence title = r.getText(titleRes);
                     CharSequence message = r.getText(
@@ -1109,10 +1072,13 @@ public class UsbDeviceManager {
                                     .extend(new Notification.TvExtender()
                                             .setChannelId(ADB_NOTIFICATION_CHANNEL_ID_TV))
                                     .build();
+                    mAdbNotificationShown = true;
                     mNotificationManager.notifyAsUser(null, id, notification,
                             UserHandle.ALL);
                 }
-                mAdbNotificationId = titleRes;
+            } else if (mAdbNotificationShown) {
+                mAdbNotificationShown = false;
+                mNotificationManager.cancelAsUser(null, id, UserHandle.ALL);
             }
         }
 
